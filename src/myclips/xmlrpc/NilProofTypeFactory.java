7462c5de40d84105fc0518c987bdb8b9997f53ed
Francesco Capozzo <ximarx@gmail.com>
2012-08-17 19:40:37 +0200

    Prima comunicazione

---
+++ src/myclips/xmlrpc/NilProofTypeFactory.java
@@ -0,0 +1,27 @@
+package myclips.xmlrpc;
+
+import org.apache.ws.commons.util.NamespaceContextImpl;
+import org.apache.xmlrpc.common.TypeFactoryImpl;
+import org.apache.xmlrpc.common.XmlRpcController;
+import org.apache.xmlrpc.common.XmlRpcStreamConfig;
+import org.apache.xmlrpc.parser.NullParser;
+import org.apache.xmlrpc.parser.TypeParser;
+import org.apache.xmlrpc.serializer.NullSerializer;
+
+public class NilProofTypeFactory extends TypeFactoryImpl {
+
+    public NilProofTypeFactory(XmlRpcController pController) {
+        super(pController);
+    }
+
+    @Override
+    public TypeParser getParser(XmlRpcStreamConfig pConfig,
+      NamespaceContextImpl pContext, String pURI, String pLocalName) {
+
+        if ("".equals(pURI) && NullSerializer.NIL_TAG.equals(pLocalName)) {
+            return new NullParser();
+        } else {
+            return super.getParser(pConfig, pContext, pURI, pLocalName);
+        }
+    }
+}
---
+++ src/myclips/xmlrpc/Wrapper.java
@@ -0,0 +1,56 @@
+package myclips.xmlrpc;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+
+
+public class Wrapper {
+
+	public Wrapper() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public static void main(String[] args) throws MalformedURLException, XmlRpcException {
+
+		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+	    config.setServerURL(new URL("http://127.0.0.1:8081/RPC2"));
+	    
+	    config.setEnabledForExtensions(true);
+	    config.setEnabledForExceptions(true);
+	    
+	    
+	    XmlRpcClient client = new XmlRpcClient();
+	    client.setConfig(config);
+	    client.setTypeFactory(new NilProofTypeFactory(client));
+	    //Object[] params = new Object[]{new Integer(33), new Integer(9)};
+	    String theToken = (String) client.execute("Sessions.new", new Object[]{});
+	    
+	    System.out.println("The token: " + theToken);
+		
+	    //Object aResult = (Object) client.execute("RemoteShell.do", new Object[]{theToken, "(+ 1 1)" });
+	    
+	    client.execute("RemoteShell.do", new Object[]{theToken, "(defrule MAIN::andata ?a <- (A B C) => (retract ?a) (assert (C B A)))" });
+	    client.execute("RemoteShell.do", new Object[]{theToken, "(defrule MAIN::ritorno ?a <- (C B A) => (retract ?a) (assert (stato ho-finito)))" });
+	    client.execute("RemoteShell.do", new Object[]{theToken, "(assert (A B C))" });
+	    
+	    client.execute("Engine.run", new Object[]{theToken});
+	    
+	    Object[] aResult = (Object[]) client.execute("Engine.getWmes", new Object[]{theToken});
+	    
+	    System.out.println("Results:");
+	    for ( Object aWme : aResult ) {
+	    	System.out.println("\t" + aWme);
+	    }
+	    
+	    
+	}
+	
+}
+
